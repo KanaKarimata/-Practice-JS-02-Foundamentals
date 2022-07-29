58bdf495fe7952b93883682b6809ca61c7ce20b1
KanaKarimata <kana.lt22@gmail.com>
2022-07-29 17:21:12 +0900

    [finish]part-2-34

--- script.js
+++ script.js
@@ -18,14 +18,30 @@ logger();
 //simple function without any parameters or return not need be in Valuable.
 //logger doesn't return any and produce any Values. It just print.
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// // if you want to use Values that was return, you need to store it in a Valuable
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+//function declaration
+function calcAge1(birthYear){
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1997);
+
+
+//function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
 }
 
-// if you want to use Values that was return, you need to store it in a Valuable
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+const age2 = calcAge2(1997);
+console.log(age1, age2);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
