1a2e4a6280c3611b46e0441e4eaf9c1f344bdac9
KanaKarimata <kana.lt22@gmail.com>
2022-07-30 10:26:24 +0900

    [finish]part-2-44

--- script.js
+++ script.js
@@ -215,7 +215,7 @@
 //   [`Michael`, `Peter`, `Steven`]
 // ];
 // //object
-// const jonas = {
+// const this = {
 //   firstName: `Jonas`,
 //   lastName: `Kana`,
 //   age: 2037 - 1997,
@@ -224,37 +224,66 @@
 // };
 // //keys: values
 // //keys are properties
-// //jonas object has 5 properties
+// //this object has 5 properties
+
+// const this = {
+//   firstName: `Jonas`,
+//   lastName: `Kana`,
+//   age: 2037 - 1997,
+//   job: `teacher`,
+//   friends: [`Michael`, `Peter`, `Steven`]
+// };
+
+// console.log(this);
+// console.log(this.lastName);
+// console.log(this[`lastName`]);
+
+// const nameKey = `Name`;
+// console.log(this[`first` + nameKey]);
+// console.log(this[`last` + nameKey]);
+
+// // console.log(this)
+
+// const interestedIn = prompt(`What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends`);
+
+// if(this[interestedIn]){
+//   console.log(this[interestedIn]);
+// } else {
+//   console.log(`Wrong request! Choose between firstName, lastName, age, job and friends`);
+// }
+
+// this.location = `Portugal`;
+// this[`twitter`] = `@jonaskarimata`;
+// console.log(this);
+
+// //challenge
+// console.log(`${this.firstName} has ${this.friends.length} friends, and his best friend is called ${this.friends[0]}`);
 
 const jonas = {
   firstName: `Jonas`,
   lastName: `Kana`,
-  age: 2037 - 1997,
+  birthYear: 1991,
   job: `teacher`,
-  friends: [`Michael`, `Peter`, `Steven`]
+  friends: [`Michael`, `Peter`, `Steven`],
+  hasDriverLicense: false,
+  calcAge: function() {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function() {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriverLicense ? `a` : `no`} driver's license.`
+  }
 };
-
-console.log(jonas);
-console.log(jonas.lastName);
-console.log(jonas[`lastName`]);
-
-const nameKey = `Name`;
-console.log(jonas[`first` + nameKey]);
-console.log(jonas[`last` + nameKey]);
-
-// console.log(jonas)
-
-const interestedIn = prompt(`What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends`);
-
-if(jonas[interestedIn]){
-  console.log(jonas[interestedIn]);
-} else {
-  console.log(`Wrong request! Choose between firstName, lastName, age, job and friends`);
-}
-
-jonas.location = `Portugal`;
-jonas[`twitter`] = `@jonaskarimata`;
-console.log(jonas);
+console.log(jonas.calcAge());
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
 
 //challenge
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+//`Jonas is a 46-year old teacher and he has a/no driver's license`
+console.log(jonas.getSummary());
+
+
+
+
+
